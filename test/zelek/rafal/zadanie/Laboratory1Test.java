9f3dcc7fefd04ef35c04bd6cb6a03a563c928a71
roen00 <roen00@gmail.com>
2015-05-10 08:38:59 +0200

    Added tests to the Laboratory1

---
+++ test/zelek/rafal/zadanie/Laboratory1Test.java
@@ -0,0 +1,57 @@
+package zelek.rafal.zadanie;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
+
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+public class Laboratory1Test {
+    @Test
+    public void labResultShouldContainsQuadraticEquationWithTwoSquares() {
+        //Given array of quadratic equation parameters
+        String[] input = {"2", "-3", "1"};
+        //When I create the Laboratory1
+        Laboratory1 lab1 = new Laboratory1(input);
+
+        //Then I expect 0.5 and 1 squares
+        List<Double> expected = Arrays.asList(0.5d, 1d);
+        assertThat(lab1.result.get().square, is(expected));
+        //And delta greater than zero
+        assertTrue(lab1.result.get().delta > 0);
+    }
+
+    @Test
+    public void labResultShouldContainsEmptyQuadraticEquationForInputArgumentsLowerThan3() {
+        //Given array of quadratic equation parameters
+        String[] input = {"2", "-3"};
+        //When I create the Laboratory1
+        Laboratory1 lab1 = new Laboratory1(input);
+        //Then I expect empty quadratic equation
+        assertThat(lab1.result, is(Optional.empty()));
+    }
+
+    @Test
+    public void labResultShouldContainsEmptyQuadraticEquationForInputArgumentsGreaterThan3() {
+        //Given array of quadratic equation parameters
+        String[] input = {"2", "-3", "4", "5"};
+        //When I create the Laboratory1
+        Laboratory1 lab1 = new Laboratory1(input);
+        //Then I expect empty quadratic equation
+        assertThat(lab1.result, is(Optional.empty()));
+    }
+
+    @Test
+    public void labResultShouldContainsEmptyQuadraticEquationForInvalidInputArguments() {
+        //Given array of quadratic equation parameters
+        String[] input = {"test", "-3", "4"};
+        //When I create the Laboratory1
+        Laboratory1 lab1 = new Laboratory1(input);
+        //Then I expect empty quadratic equation
+        assertThat(lab1.result, is(Optional.empty()));
+    }
+}
